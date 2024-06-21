31096b49dd7ec6b2551447685f38b518284252bd
setouchi-h <setouchi.kazuki@gmail.com>
2024-06-21 19:16:12 +0900

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import FoodCard from './components/FoodCard.vue'
+
+const foods = ref([
+  { name: '寿司', emoji: '🍣' },
+  { name: 'ハンバーガー', emoji: '🍔' },
+  { name: 'ラーメン', emoji: '🍜' },
+  { name: 'ピザ', emoji: '🍕' },
+  { name: 'サラダ', emoji: '🥗' },
+  { name: 'ステーキ', emoji: '🥩' },
+  { name: 'アイスクリーム', emoji: '🍨' },
+  { name: 'タコス', emoji: '🌮' },
+  { name: 'パスタ', emoji: '🍝' },
+  { name: 'ドーナツ', emoji: '🍩' }
+])
+
+const pickedFoodIndex = ref(0)
+const newFoodIndex = ref(1)
+
+function pickFood(index: number) {
+  pickedFoodIndex.value = index
+  newFoodIndex.value++
+}
+</script>
 
 <template>
-  <main><h1>今日何食べる？</h1></main>
+  <main>
+    <h1 class="title">今日何食べる？</h1>
+    <FoodCard
+      :name="foods[pickedFoodIndex].name"
+      :emoji="foods[pickedFoodIndex].emoji"
+      @click="pickFood(pickedFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foods.length">
+      <p>V.S.</p>
+      <FoodCard
+        :name="foods[newFoodIndex].name"
+        :emoji="foods[newFoodIndex].emoji"
+        @click="pickFood(newFoodIndex)"
+    /></template>
+  </main>
 </template>
 
 <style scoped>
@@ -9,5 +47,9 @@ main {
   width: 350px;
   margin: 100px auto;
   text-align: center;
+}
+
+.title {
+  margin-bottom: 60px;
 }
 </style>
---
+++ src/components/FoodCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  emoji: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="food-card">
+    <div>{{ name }}</div>
+    <div>{{ emoji }}</div>
+  </div>
+</template>
+
+<style scoped>
+.food-card {
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  font-size: 2rem;
+  padding: 16px;
+  cursor: pointer;
+}
+</style>
