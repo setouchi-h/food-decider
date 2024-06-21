ce01c83d66fa23026c9e9188dad06132cc0c459a
setouchi-h <setouchi.kazuki@gmail.com>
2024-06-21 16:08:48 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main><h1>今日何食べる？</h1></main>
+</template>
+
+<style scoped>
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
+</style>
